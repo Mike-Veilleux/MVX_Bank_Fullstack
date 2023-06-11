b296708b4c7a55808d14d7c298c8a88f3185b251
Michael Veilleux <>
2023-06-11 20:27:22 +0800

    update

---
+++ server/dist/interfaces/ENUMs.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IUserType = void 0;
+var IUserType;
+(function (IUserType) {
+    IUserType[IUserType["NONE"] = 0] = "NONE";
+    IUserType[IUserType["LOCAL"] = 1] = "LOCAL";
+    IUserType[IUserType["GOOGLE"] = 2] = "GOOGLE";
+})(IUserType = exports.IUserType || (exports.IUserType = {}));
---
+++ server/dist/routers/routerPing.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.routerPing = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+dotenv_1.default.config({ path: `.env.${process.env.NODE_ENV}` });
+exports.routerPing = express_1.default.Router();
+exports.routerPing.get("/", (req, res) => {
+    res.send(`<h1 style="text-align:center">You pinged me?</h1>`);
+});
+exports.routerPing.get("/port", (req, res) => {
+    res.send(`<h1 style="text-align:center">You pinged me? ${process.env.PORT}</h1>`);
+});
